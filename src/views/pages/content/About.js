3ca3fc77498bc5e6d3c1f37a2bf175ef88e9fed9
Rxheem McDonald <raheemdevelopment@outlook.com>
2019-05-05 12:30:52 -0500

    Updates,

--- src/views/pages/content/About.js
+++ src/views/pages/content/About.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Page from '../essentials/Page';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 const About = props => {
   return (
     <Page fluid={false} header='About' description=''>
-      <div />
+      <Row>
+        <Col>Left</Col>
+        <Col>
+          Right
+        </Col>
+      </Row>
     </Page>
   );
 };
